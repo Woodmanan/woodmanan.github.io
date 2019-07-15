f71e7f6afc73a7f9c01ad81f71bef6b1d87b65f2
Woodmanan <mwmccoy@andrew.cmu.edu>
2019-07-14 20:53:24 -0400

    First running Ps.js script

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-
+  createCanvas(displayWidth, 800);
 }
 
 function draw() {
+  background(0);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
   ellipse(mouseX, mouseY, 80, 80);
 }
